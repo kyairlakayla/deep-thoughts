a3d5bf6c38fc28fb4c30b0db673f1d31b0db6ff3
Kayla <kyairlakayla.davis@gmail.com>
2021-11-05 18:08:25 -0400

    pushing with issue

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +14,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+  //  context: authMiddleware
   });
   // start Apollo server
   await server.start();
