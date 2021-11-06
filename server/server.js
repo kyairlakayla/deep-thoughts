72e37ba44ff9237b5089b5ff66ca1522562379b1
Kayla <kyairlakayla.davis@gmail.com>
2021-11-06 11:02:26 -0400

    connect client and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -27,6 +28,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 startServer();
+
+// serve static assets 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
